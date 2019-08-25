e5ade3a3c130678fe991531d2d4b21a46339b924
Omor Faruque <mdomor.faruque@rci.rogers.ca>
2019-08-24 23:25:19 -0400

    xx commit

--- src/main/java/com/omor/tutorial/aws_s3/App.java
+++ src/main/java/com/omor/tutorial/aws_s3/App.java
@@ -7,22 +7,27 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args)
 
 	{
 		Regions region = Regions.CA_CENTRAL_1;
 		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(region).build();
+		createS3Bucket(s3Client);
 		listingS3Buckets(region, s3Client);
-		
-		
 
 	}
-	
+
+	public static void createS3Bucket(AmazonS3 s3Client) {
+		String bucketName = "tutorial-bucket-2019";
+		
+		if (s3Client.doesBucketExist(bucketName)) {
+			System.out.println("Bucket name is not available. Try again with a different Bucket name.");
+			return;
+		}
+		s3Client.createBucket(bucketName);
+	}
+
 	public static void listingS3Buckets(Regions region, AmazonS3 s3Client) {
 		List<Bucket> buckets = s3Client.listBuckets();
 		for (Bucket bucket : buckets) {
