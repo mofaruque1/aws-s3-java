e66f8747a24245b557af0c8ea200a5f388888b44
Omor Faruque <mdomor.faruque@rci.rogers.ca>
2019-08-24 23:34:32 -0400

    xx commit

--- src/main/java/com/omor/tutorial/aws_s3/App.java
+++ src/main/java/com/omor/tutorial/aws_s3/App.java
@@ -2,6 +2,7 @@ package com.omor.tutorial.aws_s3;
 
 import java.util.List;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -12,15 +13,34 @@ public class App {
 
 	{
 		Regions region = Regions.CA_CENTRAL_1;
+		String bucketName = "tutorial-bucket-2019";
 		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(region).build();
-		createS3Bucket(s3Client);
+		
+		System.out.println("|---------listing buckets---------|");
 		listingS3Buckets(region, s3Client);
+		
+		System.out.println("|---------creating bucket---------|");
+		createS3Bucket(s3Client,bucketName);
+		
+		System.out.println("|---------listing buckets---------|");
+		listingS3Buckets(region, s3Client);
+		
+		System.out.println("|---------delete bucket---------|");
+		deleteS3Bucket(s3Client,bucketName);
+		
+		System.out.println("|---------listing buckets---------|");
+		listingS3Buckets(region, s3Client);
+		
 
 	}
 
-	public static void createS3Bucket(AmazonS3 s3Client) {
-		String bucketName = "tutorial-bucket-2019";
+	/**
+	 * @param s3Client
+	 *            Amazons3 this function creates a s3 bucket
+	 */
+	public static void createS3Bucket(AmazonS3 s3Client,String bucketName) {
 		
+
 		if (s3Client.doesBucketExist(bucketName)) {
 			System.out.println("Bucket name is not available. Try again with a different Bucket name.");
 			return;
@@ -28,6 +48,25 @@ public class App {
 		s3Client.createBucket(bucketName);
 	}
 
+	/**
+	 * @param bucketname
+	 *            Deletes a s3 bucket
+	 */
+	public static void deleteS3Bucket(AmazonS3 s3Client,String bucketname) {
+		try {
+			s3Client.deleteBucket(bucketname);
+		} catch (AmazonServiceException e) {
+			System.err.println(e.getErrorMessage());
+			return;
+		}
+	}
+
+	/**
+	 * @param region
+	 *            the region on your console
+	 * @param s3Client
+	 *            Amazons3 list all the bucket with this account
+	 */
 	public static void listingS3Buckets(Regions region, AmazonS3 s3Client) {
 		List<Bucket> buckets = s3Client.listBuckets();
 		for (Bucket bucket : buckets) {
